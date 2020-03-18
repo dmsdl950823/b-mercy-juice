f1f4ce7f31ff467af98c8720d433ed9c406e9ea6
root <root@localhost.localdomain>
2020-03-18 11:06:59 +0900

    Updates

---
+++ precache-manifest.9b7ea5a07d0e6b01dcb52e0a8bb4b909.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "050deac297f0efaf1dbc011fd57f22e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d9b27a710fed9a7feb3",
+    "url": "/static/css/main.305e470b.chunk.css"
+  },
+  {
+    "revision": "6682153f8041ed6b4b45",
+    "url": "/static/js/2.0ee106d3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.0ee106d3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8d9b27a710fed9a7feb3",
+    "url": "/static/js/main.86666f12.chunk.js"
+  },
+  {
+    "revision": "122cf81b112056e92f8f",
+    "url": "/static/js/runtime-main.760204cc.js"
+  },
+  {
+    "revision": "0f31309d112f8574293ade0e51411859",
+    "url": "/static/media/bck_img1.0f31309d.jpg"
+  },
+  {
+    "revision": "ad34c9f15f5f99445fc09d4e54370f36",
+    "url": "/static/media/bck_img2.ad34c9f1.jpg"
+  },
+  {
+    "revision": "667440e131489ceae5e8b624d92da946",
+    "url": "/static/media/bck_img3.667440e1.jpg"
+  },
+  {
+    "revision": "f15a12cb8c3b37ae1ab85323042ecf00",
+    "url": "/static/media/logo.f15a12cb.png"
+  }
+]);
